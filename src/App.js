c461ec6e55854ca96b936e79f413a4f34cc3c628
arddnc <139110435+arddnc@users.noreply.github.com>
2026-04-09 16:21:08 +0300

    Update App.js - wrap /products with PrivateRoute for auth protection

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import ProductList from "./pages/ProductList";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/products" />} />
+    return (
+          <Router>
+            <Routes>
+              <Route path="/" element={<Navigate to="/products" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
-        <Route path="/products" element={<ProductList />} />
-      </Routes>
-    </Router>
+        <Route
+          path="/products"
+          element={
+                        <PrivateRoute>
+                          <ProductList />
+            </PrivateRoute>
+}
+        />
+          </Routes>
+          </Router>
   );
 }
 
