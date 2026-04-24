d17a788cae687637b709db7c9d75c35a25630e1b
İnci Lal Dikmen <lal.dikmen@sabanciuniv.edu>
2026-04-24 13:51:14 +0300

    Add product detail API service

--- src/services/products.js
+++ src/services/products.js
@@ -10,4 +10,14 @@ async function getProducts() {
   return response.json();
 }
 
+export async function getProductById(productId) {
+  const response = await fetch(`${API_URL}/${productId}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch product details");
+  }
+
+  return response.json();
+}
+
 export default getProducts;
